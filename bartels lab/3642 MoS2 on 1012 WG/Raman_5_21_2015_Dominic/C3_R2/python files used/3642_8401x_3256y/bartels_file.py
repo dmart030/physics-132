77dde376bb232ff69af233cd9a9fe213a82abc95
dmart030 <dmart030@ucr.edu>
2015-05-28 01:08:25 -0700

    I cri half the tiem

--- bartels lab/3642 MoS2 on 1012 WG/Raman_5_21_2015_Dominic/C3_R2/python files used/3642_8401x_3256y/bartels_file.py
+++ bartels lab/3642 MoS2 on 1012 WG/Raman_5_21_2015_Dominic/C3_R2/python files used/3642_8401x_3256y/bartels_file.py
@@ -8,14 +8,14 @@ Created on Thu May 21 15:56:57 2015
 import numpy as np
 import matplotlib.pyplot as plt
 
-DataIN = np.loadtxt('3642 8432x 3259y PL 600 T2 D1 100x.txt')
+DataIN = np.loadtxt('3642 8432x 3298y PL 600 T2 D1 100x.txt')
 y = []
 v = []
 for column in DataIN:
     v.append(column[1])
     y.append(column[0])
-plt.plot(y,v)
+plt.plot(y,v, 'b')
 plt.ylabel('Intensity')
-plt.title('PL')
-plt.xlabel('eV')
-plt.savefig('3259.jpg')
+plt.title('PL Spectra')
+plt.xlabel('Energy [eV]')
+plt.savefig('3298PL.jpg')
